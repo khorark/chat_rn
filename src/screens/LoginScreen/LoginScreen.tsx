71a8c1255830ac35aa131244f993d6b88a3e9ae9
Khoroshikh Arkadiy <khorark@gmail.com>
2018-11-11 20:16:15 +0800

    LoginScreen. Добавлено отображение индикатора pin кода

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -2,7 +2,7 @@
  * Created by arkadiy on 11.11.18.
  */
 import React, { PureComponent } from 'react'
-import { ScrollView } from 'react-native'
+import { View } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 import Image from 'react-native-remote-svg'
 
@@ -19,12 +19,23 @@ export default class LoginScreen extends PureComponent<{}, ILoginScreenState> {
     }
 
     public render() {
+        const { pass } = this.state
+
         return (
-            <ScrollView contentContainerStyle={styles.scrollContainer}>
-                <LinearGradient colors={[colors.turquoiseapprox, colors.iceCold]} style={styles.mainContainer}>
-                    <Image source={require('../../assets/images/logo.svg')} style={styles.logoImage}/>
+            <View style={styles.mainContainer}>
+                <LinearGradient colors={[colors.turquoiseapprox, colors.iceCold]} style={styles.lineGDContainer}>
+                    <View style={styles.imageContainer}>
+                        <Image source={require('../../assets/images/logo.svg')} style={styles.logoImage}/>
+                    </View>
+                    <View style={styles.centerContainer}>
+                        <View style={styles.inputContainer}>
+                            {Array.from(Array(4).keys()).map((key: number) => (
+                                <View key={key} style={[styles.circle, pass.length > key ? styles.circleFill : null]}/>
+                            ))}
+                        </View>
+                    </View>
                 </LinearGradient>
-            </ScrollView>
+            </View>
         )
     }
 }
--- src/screens/LoginScreen/LoginScreenStyles.tsx
+++ src/screens/LoginScreen/LoginScreenStyles.tsx
@@ -2,14 +2,38 @@ import EStyleSheet from 'react-native-extended-stylesheet'
 import { getRemValue } from '../../helpers/stylesHelper'
 
 export default EStyleSheet.create({
-    scrollContainer: {
+    mainContainer: {
         flexGrow: 1,
     },
-    mainContainer: {
+    lineGDContainer: {
         flex: 1,
+    },
+    imageContainer: {
+        marginTop: getRemValue(36),
+        marginLeft: getRemValue(30),
     },
     logoImage: {
         width: getRemValue(235),
         height: getRemValue(69),
+    },
+    centerContainer: {
+        alignItems: 'center',
+        marginTop: getRemValue(72),
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: getRemValue(30),
+    },
+    circle: {
+        width: getRemValue(14),
+        height: getRemValue(14),
+        borderRadius: getRemValue(7),
+        borderWidth: 2,
+        borderColor: '#fff',
+        marginHorizontal: getRemValue(14),
+    },
+    circleFill: {
+        backgroundColor: '#fff',
     },
 })
