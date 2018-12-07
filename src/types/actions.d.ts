31dd0343d029db5caa033a2ad038813dd31a010e
Khoroshikh Arkadiy <khorark@gmail.com>
2018-12-07 19:59:25 +0300

    Добавлены дополнительные hooks по предобработке файла и линтинга

--- src/types/actions.d.ts
+++ src/types/actions.d.ts
@@ -2,33 +2,32 @@ import { Action, AnyAction } from 'redux'
 import { Type } from '../redux/actions'
 import { IMessage } from './reducer'
 
-
 export interface IAddMessage extends Action {
     type: Type.ADD_MESSAGE
     message: {
         id: number
-        author: string,
-        text: string,
-        date: Date,
-    },
+        author: string
+        text: string
+        date: Date
+    }
 }
 
 export interface IRemoveMessage extends Action {
     type: Type.REMOVE_MESSAGE
-    id: number,
+    id: number
 }
 
 export interface ILoading extends Action {
-    type: Type.IS_LOADING,
-    isLoading: boolean,
+    type: Type.IS_LOADING
+    isLoading: boolean
 }
 
 export interface IAddMessagePayload {
-    message: IMessage,
+    message: IMessage
 }
 
 export interface IRemoveMessagePayload {
-    id: number,
+    id: number
 }
 
 export type ChatActions = IAddMessage | IRemoveMessage | ILoading
--- src/types/reducer.d.ts
+++ src/types/reducer.d.ts
@@ -1,11 +1,11 @@
 export interface IReduxState {
-    messages: IMessage[],
-    isLoading: boolean,
+    messages: IMessage[]
+    isLoading: boolean
 }
 
 export interface IMessage {
-    id: number,
-    author: string,
-    text: string,
-    date: Date,
+    id: number
+    author: string
+    text: string
+    date: Date
 }
