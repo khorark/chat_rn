3e6aa1f473fd1a5e1b172532367b6ab98c57bda7
Khoroshikh Arkadiy <khorark@gmail.com>
2019-01-08 18:02:53 +0300

    storageHelper. Доработки по ассинхронного чтения и записи данных в storage

--- src/helpers/storageHelper.ts
+++ src/helpers/storageHelper.ts
@@ -4,21 +4,16 @@ import { AsyncStorage } from 'react-native'
  * Функция для получения данных из хранилища
  * @param valueName название переменной
  */
-export const getValueFromStorage = async (valueName: string) => {
-    return await AsyncStorage.getItem(valueName)
-}
+export const getValueFromStorage = async (valueName: string) => AsyncStorage.getItem(valueName)
 
 /**
  * Функция для сохранения данных в хранилище
  * @param valueName название переменной
  * @param value значение переменной
  */
-export const setValueToStorage = async (valueName: string, value: string ) => {
-    return await AsyncStorage.setItem(valueName, value)
-}
+export const setValueToStorage = async (valueName: string, value: string) => AsyncStorage.setItem(valueName, value)
 
 /**
  * Проверка, есть ли данные в хранилище
  */
-export const checkDataStorage = async (valueName: string) => !!await getValueFromStorage(valueName)
-
+export const checkDataStorage = async (valueName: string) => !!(await getValueFromStorage(valueName))
