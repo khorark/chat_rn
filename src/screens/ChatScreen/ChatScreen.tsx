5158e56494e28c2bf447f6827f6630d8916fcdcf
Khoroshikh Arkadiy <khorark@gmail.com>
2019-01-08 18:03:36 +0300

    ChatScreen. Добавлен шаблон скрина

---
+++ src/screens/ChatScreen/ChatScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+const ChatScreen = () => <View style={styles.mainContainer} />
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flexGrow: 1,
+        backgroundColor: 'green',
+    },
+})
+
+export default ChatScreen
